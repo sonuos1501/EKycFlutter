1ec29cbb54c8f82a861d47f3290d96f2b8d99f1b
sonuos1501 <son.uospt@gmail.com>
2024-12-16 11:12:36 +0700

    brigde native

--- android/src/main/kotlin/com/son/ekycflutter/EkycflutterPlugin.kt
+++ android/src/main/kotlin/com/son/ekycflutter/EkycflutterPlugin.kt
@@ -1,12 +1,9 @@
 package com.son.ekycflutter
 
-import androidx.annotation.NonNull
-
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
-import io.flutter.plugin.common.MethodChannel.Result
 
 /** EkycflutterPlugin */
 class EkycflutterPlugin: FlutterPlugin, MethodCallHandler {
@@ -14,9 +11,23 @@ class EkycflutterPlugin: FlutterPlugin, MethodCallHandler {
   ///
   /// This local reference serves to register the plugin with the Flutter Engine and unregister it
   /// when the Flutter Engine is detached from the Activity
+
+  companion object {
+    private var instance: EkycflutterPlugin? = null
+    const val KEY_SEND_EVENT: String = "KEY_SEND_EVENT"
+
+    fun getInstance(): EkycflutterPlugin {
+        return instance ?: throw IllegalStateException("EkycflutterPlugin is not initialized")
+    }
+  }
+
+  public mEvent: EventChanel.EventSink? = null
+  public mainCallBack: OnMai
+
   private lateinit var channel : MethodChannel
 
   override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+    instance = this
     channel = MethodChannel(flutterPluginBinding.binaryMessenger, "ekycflutter")
     channel.setMethodCallHandler(this)
   }
@@ -31,5 +42,16 @@ class EkycflutterPlugin: FlutterPlugin, MethodCallHandler {
 
   override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
     channel.setMethodCallHandler(null)
+    instance = null
   }
+
+  fun sendEvent(map: HashMap<String, String>) {
+    mainCallBack?.doAction(KEY_SEND_EVENT, map)
+  }
+
+
+  interface OnMainCallBack {
+    fun doAction(key: String, data: Any)
+  }
+
 }
